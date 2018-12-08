a0478f9480d0cacd275946073a40e4bd278c04aa
shensicle <shensicle@gmail.com>
2018-12-07 21:03:16 -0500

    IFTTT interface cleanup

--- libraries/IFTTTMessage/IFTTTMessage.cpp
+++ libraries/IFTTTMessage/IFTTTMessage.cpp
@@ -28,7 +28,6 @@ bool IFTTTMessageClass::Connect (void)
    // Value which, when set, indicates connection to IFTTT server was successful
    bool returnValue = true;
    
-   Serial.println("Connecting to IFTTT...");
    if(TheClient.connect("maker.ifttt.com",80))  // Test the connection to the server
    {
      Serial.println("Connected to ifttt");
@@ -46,44 +45,21 @@ bool IFTTTMessageClass::Connect (void)
 // Send a message. Return value indicates whether or not message was successfully sent
 bool IFTTTMessageClass::Send (String theMessage)
 {
-    // Packet will end up looking similar to this:
-  // 
-  //  POST /trigger/ESP/with/key/(myKey) HTTP/1.1
-  //  Host: maker.ifttt.com
-  //  User-Agent: ESP32_Thing
-  //  Connection: close
-  //  Conent-Type: application/json
-  //  Conent-Length: (postData length)
-  //  
-  //  {"value1":"(HIGH/LOW )"}
-  
-  
+    
     if (Connect())
     {
     	// Note that ifttt only supports labels value1, value2, value3
-  	    String postData = PostString;
+  	    String postData;
   	    postData.concat ("{\"value1\":\"");
    	    postData.concat (SensorID);
   	    postData.concat ("\",\"value2\":\"");
   	    postData.concat(theMessage);
   	    postData.concat("\"}");
-    
 
-//  TheClient.print("POST /trigger/");
-//  TheClient.print("ESP");
-//  TheClient.print("/with/key/");
-//  TheClient.print(APIKey);
-//  TheClient.println(" HTTP/1.1");
-
-//  TheClient.println("Host: maker.ifttt.com");
-//  TheClient.println("User-Agent: WaterSensor");
-//  TheClient.println("Connection: close");
-
-//  TheClient.println("Content-Type: application/json");
-//  TheClient.print("Content-Length: ");
-      TheClient.println(postData.length());
-      TheClient.println();
-      TheClient.println(postData);
+  	    TheClient.print (PostString);          // Connection details
+	    TheClient.println(postData.length());  // length of JSON payload
+        TheClient.println();
+        TheClient.println(postData);           // JSON payload
     }
 }
 
